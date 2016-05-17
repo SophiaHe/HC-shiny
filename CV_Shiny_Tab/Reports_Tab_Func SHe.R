133c892c3732e16ea1fad9bd32a433559943b9fe
SophiaHe <hyq9265@gmail.com>
2016-05-17 14:12:57 -0400

    Fetch & merge tables from db remotely and as.data.frame the final results

--- CV_Shiny_Tab/Reports_Tab_Func SHe.R
+++ CV_Shiny_Tab/Reports_Tab_Func SHe.R
@@ -87,53 +87,44 @@ reports_tab <- function(current_generic, current_brand,current_rxn, date_ini, da
   #    count the amount of this report & elimiate them when calculating porprotions!!!!!!!!
 
   cv_drug_product_ingredients_rp <-  if(is.na(current_generic) == FALSE){
-                                         as.data.frame(
                                             tbl(hcopen, sql("SELECT * FROM cv_drug_product_ingredients")) %>%
                                             select(DRUG_PRODUCT_ID, ACTIVE_INGREDIENT_NAME) %>%
-                                            filter(ACTIVE_INGREDIENT_NAME == current_generic),
-                                         n=-1)
+                                            filter(ACTIVE_INGREDIENT_NAME == current_generic)
                                      } else {
-                                       as.data.frame(
                                          tbl(hcopen, sql("SELECT * FROM cv_drug_product_ingredients")) %>%
-                                           select(DRUG_PRODUCT_ID, ACTIVE_INGREDIENT_NAME),
-                                       n=-1)
+                                           select(DRUG_PRODUCT_ID, ACTIVE_INGREDIENT_NAME)
                                      }
     
   cv_report_drug_rp <- if(is.na(current_brand) == FALSE){
-                          as.data.frame(
                             tbl(hcopen,sql("SELECT * FROM cv_report_drug")) %>%
                             select(REPORT_ID,DRUG_PRODUCT_ID,DRUGNAME) %>%
-                            filter(DRUGNAME == current_brand),
-                          n=-1)
+                            filter(DRUGNAME == current_brand)
                        } else {
-                         as.data.frame(
                            tbl(hcopen,sql("SELECT * FROM cv_report_drug")) %>%
-                             select(REPORT_ID,DRUG_PRODUCT_ID,DRUGNAME),
-                           n=-1)
+                             select(REPORT_ID,DRUG_PRODUCT_ID,DRUGNAME)
                        }
-  cv_report_drug_rp <- cv_report_drug_rp[order(cv_report_drug_rp$DRUG_PRODUCT_ID),]  
+  # cv_report_drug_rp <- cv_report_drug_rp[order(cv_report_drug_rp$DRUG_PRODUCT_ID),]  
   
   cv_reactions_rp <- if(is.na(current_rxn) == FALSE){
-                        as.data.frame(
                           tbl(hcopen,sql("SELECT * FROM cv_reactions")) %>%
                           select(REPORT_ID, PT_NAME_ENG) %>%
-                          filter(PT_NAME_ENG == current_rxn),
-                        n=-1)
+                          filter(PT_NAME_ENG == current_rxn)
                      } else {
-                       as.data.frame(
                          tbl(hcopen,sql("SELECT * FROM cv_reactions")) %>%
-                           select(REPORT_ID, PT_NAME_ENG),
-                         n=-1)
+                           select(REPORT_ID, PT_NAME_ENG)
                      }
   
   reports_tab_master <- cv_drug_product_ingredients_rp %>%
                         left_join(cv_drug_product_ingredients_rp) %>%
                         left_join(cv_report_drug_rp) %>%
                           filter(REPORT_ID != "NA") %>% # some drugs will have the same ingredient but the durg name doesn't match current_brand
-                        left_join(cv_reports_sorted_rp) %>%
-                          filter(as.character(DATINTRECEIVED_CLEAN) != "NA")%>% # DATINTRECEIVED_CLEAN = NA means there're not within searched time range
                         left_join(cv_reactions_rp) %>%
-                          filter(PT_NAME_ENG == current_rxn)
+                          filter(PT_NAME_ENG == current_rxn) %>%
+                        as.data.frame(n=-1)
+  reports_tab_master <- reports_tab_master %>% 
+                        left_join(cv_reports_sorted_rp) %>%
+                          filter(as.character(DATINTRECEIVED_CLEAN) != "NA") # DATINTRECEIVED_CLEAN = NA means there're not within searched time range
+  
   #return(as.data.frame(reports_tab_master))  
 }
 
