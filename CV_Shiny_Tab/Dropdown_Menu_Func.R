249556aafc31d40f181843b435bb74a3106f247f
SophiaHe <hyq9265@gmail.com>
2016-05-25 16:33:44 -0400

    tbl eliminated & outputs are in data table format

--- CV_Shiny_Tab/Dropdown_Menu_Func.R
+++ CV_Shiny_Tab/Dropdown_Menu_Func.R
@@ -8,17 +8,17 @@ topingd_func <- function(n){
   # connect to CV database
   hcopen <- src_postgres(host = "shiny.hc.local", user = "hcreader", dbname = "hcopen", password = "canada1")
   
-  cv_reports_dm <- tbl(hcopen,"cv_reports") %>% select(REPORT_ID)
+  cv_reports_dm <- cv_reports %>% select(REPORT_ID)
   
-  cv_report_drug_dm <- tbl(hcopen, "cv_report_drug") %>%
+  cv_report_drug_dm <- cv_report_drug %>%
     select(REPORT_ID, DRUG_PRODUCT_ID)
   
-  cv_drug_product_ingredients_dm <- tbl(hcopen, "cv_drug_product_ingredients") %>%
+  cv_drug_product_ingredients_dm <- cv_drug_product_ingredients %>%
     select(DRUG_PRODUCT_ID, ACTIVE_INGREDIENT_NAME)
   
   ingd_master <-  cv_reports_dm %>% left_join(cv_report_drug_dm) %>% left_join(cv_drug_product_ingredients_dm) 
   
-  topingd <-  dplyr::summarise(group_by(ingd_master, ACTIVE_INGREDIENT_NAME),count=n_distinct(REPORT_ID)) %>% as.data.frame()
+  topingd <-  dplyr::summarise(group_by(ingd_master, ACTIVE_INGREDIENT_NAME),count=n_distinct(REPORT_ID)) %>% as.data.table()
   topingd_final <-  topingd %>% arrange(desc(count)) %>% top_n(n) %>% select(ACTIVE_INGREDIENT_NAME)
 }
 
@@ -26,14 +26,14 @@ topdrug_func <- function(n){
   # connect to CV database
   hcopen <- src_postgres(host = "shiny.hc.local", user = "hcreader", dbname = "hcopen", password = "canada1")
   
-  cv_reports_dm <- tbl(hcopen, "cv_reports") %>% select(REPORT_ID)
+  cv_reports_dm <- cv_reports %>% select(REPORT_ID)
   
-  cv_report_drug_dm <- tbl(hcopen, "cv_report_drug") %>%
+  cv_report_drug_dm <- cv_report_drug %>%
     select(REPORT_ID, DRUGNAME)
   
   drugs_master <-cv_reports_dm %>% left_join(cv_report_drug_dm) 
   
-  topdrugs <- dplyr::summarise(group_by(drugs_master, DRUGNAME),count=n_distinct(REPORT_ID)) %>% as.data.frame()
+  topdrugs <- dplyr::summarise(group_by(drugs_master, DRUGNAME),count=n_distinct(REPORT_ID)) %>% as.data.table()
   topdrugs_final <- topdrugs %>% arrange(desc(count)) %>% top_n(n) %>% select(DRUGNAME)
 } 
 
@@ -41,15 +41,15 @@ toprxn_func <- function(n){
   # connect to CV database
   hcopen <- src_postgres(host = "shiny.hc.local", user = "hcreader", dbname = "hcopen", password = "canada1")
   
-  cv_reports_dm <- tbl(hcopen, "cv_reports") %>% select(REPORT_ID)
+  cv_reports_dm <- cv_reports %>% select(REPORT_ID)
   #cv_reports_dm1 <- as.data.frame(cv_reports_dm,n=-1)
   
-  cv_reactions_dm <- tbl(hcopen, "cv_reactions") %>% select(REPORT_ID, PT_NAME_ENG)
+  cv_reactions_dm <- cv_reactions %>% select(REPORT_ID, PT_NAME_ENG)
   #cv_reactions_dm1 <- as.data.frame(cv_reactions_dm,n=-1)
   
   rxn_master <- cv_reports_dm %>% left_join(cv_reactions_dm) 
   
-  toprxn <- dplyr::summarise(group_by(rxn_master, PT_NAME_ENG),count=n_distinct(REPORT_ID)) %>% as.data.frame()
+  toprxn <- dplyr::summarise(group_by(rxn_master, PT_NAME_ENG),count=n_distinct(REPORT_ID)) %>% as.data.table()
   toprxn_final <- toprxn %>% arrange(desc(count)) %>% top_n(n) %>% select(PT_NAME_ENG)
   
   #test <- cv_reports_dm1 %>% left_join(cv_reactions_dm1)
