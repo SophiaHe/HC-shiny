944537625d258d8036e982209948739581866246
SophiaHe <hyq9265@gmail.com>
2016-05-26 17:44:47 -0400

    Function includes current_brand & current_rxn

--- CV_Shiny_DrugTabOnly/DO_Reports_Tab_Func SHe.R
+++ CV_Shiny_DrugTabOnly/DO_Reports_Tab_Func SHe.R
@@ -3,22 +3,23 @@
 #              Report_Drug (REPORT_ID, DRUGNAME)
 
 #current_brand <- "REMICADE"
-#date_ini <- ymd("19730601")
+#current_rxn <- "DRUG INEFFECTIVE"
+#date_ini <- ymd("19650601")
 #date_end <- ymd("20160526")
 
-reports_tab <- function(current_brand, date_ini, date_end) { 
+reports_tab <- function(current_brand,current_rxn,date_ini, date_end) { 
   # connect to CV database
   hcopen <- src_postgres(host = "shiny.hc.local", user = "hcreader", dbname = "hcopen", password = "canada1")
-  
+  escape.POSIXt <- dplyr:::escape.Date
   #cv_reports <- tbl(hcopen, "cv_reports") 
   #cv_report_drug <- tbl(hcopen,"cv_report_drug")
+  #cv_reactions <- tbl(hcopen,"cv_reactions") 
   
-  cv_reports_sorted_rp <- 
-    cv_reports %>%
-    select(REPORT_ID, SERIOUSNESS_ENG, REPORTER_TYPE_ENG, DEATH, DISABILITY, CONGENITAL_ANOMALY,LIFE_THREATENING, HOSP_REQUIRED, 
-           OTHER_MEDICALLY_IMP_COND, DATINTRECEIVED_CLEAN) %>%
-    filter(DATINTRECEIVED_CLEAN >= date_ini) %>%
-    filter(DATINTRECEIVED_CLEAN <= date_end)
+  cv_reports_sorted_rp <- cv_reports %>%
+                          select(REPORT_ID, SERIOUSNESS_ENG, REPORTER_TYPE_ENG, DEATH, DISABILITY, CONGENITAL_ANOMALY,LIFE_THREATENING, HOSP_REQUIRED, 
+                                 OTHER_MEDICALLY_IMP_COND, DATINTRECEIVED_CLEAN) %>%
+                          filter(DATINTRECEIVED_CLEAN >= date_ini) %>%
+                          filter(DATINTRECEIVED_CLEAN <= date_end)
   
   cv_report_drug_rp <- if(is.na(current_brand) == FALSE){
                         cv_report_drug %>%
@@ -28,10 +29,21 @@ reports_tab <- function(current_brand, date_ini, date_end) {
                         cv_report_drug %>%
                         select(REPORT_ID, DRUGNAME)
                       }
-  
+  cv_reactions_rp <- if(is.na(current_rxn) == FALSE){
+                        cv_reactions %>%
+                          select(REPORT_ID, PT_NAME_ENG) %>%
+                          filter(PT_NAME_ENG == current_rxn)
+                      } else {
+                        cv_reactions %>%
+                          select(REPORT_ID, PT_NAME_ENG)
+                      }
+  #ptm <- proc.time()
   reports_tab_master <-  cv_reports_sorted_rp%>%
-    inner_join(cv_report_drug_rp) %>%
-    as.data.table(n=-1) 
+                          semi_join(cv_report_drug_rp) %>%
+                          semi_join(cv_reactions_rp) %>%
+                          as.data.table(n=-1) 
+  
+  #proc.time() - ptm
   
   return(reports_tab_master) 
 }
