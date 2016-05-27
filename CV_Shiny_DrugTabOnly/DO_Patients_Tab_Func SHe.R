4755d2d2fd0db02bd121951c2c9ff36e7616f360
SophiaHe <hyq9265@gmail.com>
2016-05-27 17:25:41 -0400

    Performance improvement needed

--- CV_Shiny_DrugTabOnly/DO_Patients_Tab_Func SHe.R
+++ CV_Shiny_DrugTabOnly/DO_Patients_Tab_Func SHe.R
@@ -2,9 +2,9 @@
 #              Report_Drug (REPORT_ID, DRUGNAME)
 
 
-patients_tab <- function( current_brand, current_rxn,date_ini, date_end) { 
+patients_tab <- function( current_brand, current_rxn,current_date_range) { 
   # connect to CV database
-  hcopen <- src_postgres(host = "shiny.hc.local", user = "hcreader", dbname = "hcopen", password = "canada1")
+  #hcopen <- src_postgres(host = "shiny.hc.local", user = "hcreader", dbname = "hcopen", password = "canada1")
   
   #cv_reports <- tbl(hcopen, "cv_reports") 
   #cv_report_drug <- tbl(hcopen,"cv_report_drug")
@@ -12,8 +12,9 @@ patients_tab <- function( current_brand, current_rxn,date_ini, date_end) {
   # Import tables with particular search items with method to deal with unspecified search term
   cv_reports_sorted_pt <- cv_reports %>%
                           select(REPORT_ID, DATINTRECEIVED_CLEAN, GENDER_ENG, AGE_GROUP_CLEAN) %>%
-                          filter(DATINTRECEIVED_CLEAN >= date_ini) %>%
-                          filter(DATINTRECEIVED_CLEAN <= date_end)
+                          filter(DATINTRECEIVED_CLEAN >= current_date_range[1], DATINTRECEIVED_CLEAN <= current_date_range[2])
+                          #filter(DATINTRECEIVED_CLEAN >= date_ini) %>%
+                          #filter(DATINTRECEIVED_CLEAN <= date_end)
   
   
   cv_report_drug_pt <- if(is.na(current_brand) == FALSE){
@@ -37,7 +38,7 @@ patients_tab <- function( current_brand, current_rxn,date_ini, date_end) {
   patients_tab_master <-cv_reports_sorted_pt%>%
                           semi_join(cv_report_drug_pt) %>%
                           semi_join(cv_reactions_pt) %>%
-                          as.data.table(n=-1)
+                          collect()
 
   return(patients_tab_master) 
 }
